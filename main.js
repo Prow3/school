22089e3205e1cb410c65c52c7171ba5836da761a
SenkoSan <whchdjfncbncnfndj@gmail.com>
2023-04-25 22:05:45 +0300

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-// вариант 1
-const button = document.getElementById("ok"); // тут мы задаём константу button значением butoon (или элементом, не помню что это)
+const button = document.getElementById("ok");
 let h2res = document.getElementById("res").innerHTML;
 function v1() {
-  // здесь создаём функцыю которая будет использоватся при нажатии кнопки
-
   let speed = document.getElementById("speed").value;
   let time = document.getElementById("time").value;
   let res = speed * time;
@@ -18,9 +15,6 @@ function v1() {
     throw Error;
   }
 
-  // console.log(speed);
-
-  // alert(res);
   document.getElementById("res").innerHTML = h2res + res;
 }
-button.addEventListener("click", v1); // не знаю как это коментировать но если коротко то это штука которая при нажатии кнопки вызывает функцию
+button.addEventListener("click", v1);
