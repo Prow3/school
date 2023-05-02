11e338779f45fd53c5247a9a3f4866a0cc74cce8
SenkoSan <whchdjfncbncnfndj@gmail.com>
2023-05-02 20:09:46 +0300

    deleted:    2023-04-24_10-28-18.mp3 	modified:   main.js

--- main.js
+++ main.js
@@ -12,7 +12,13 @@ function v1() {
   document.getElementById("res").innerHTML = res;
   var audio = new Audio();
   audio.preload = "auto";
-  audio.src = "/2023-04-24_10-28-18.mp3";
+  audio.src =
+    "https://cdn.discordapp.com/attachments/894902167314501633/1103005312652427264/2023-04-24_10-28-18.mp3";
+  audio.play();
+  var audio = new Audio();
+  audio.preload = "auto";
+  audio.src =
+    "https://cdn.discordapp.com/attachments/894902167314501633/1103005130657378405/padenie-truby.mp3";
   audio.play();
 }
 button.addEventListener("click", v1);
