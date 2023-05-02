26fdffcc47da225f976a82057c8ed9bda67841a7
SenkoSan <whchdjfncbncnfndj@gmail.com>
2023-05-02 13:56:45 +0300

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -1,20 +1,14 @@
 const button = document.getElementById("ok");
-const h2res = document.getElementById("res").innerHTML;
 function v1() {
-  let speed = document.getElementById("speed").value;
-  let time = document.getElementById("time").value;
-  let res = speed * time;
-  speed = Number(speed);
-  time = Number(time);
-  if (speed <= 0) {
-    document.getElementById("res").innerHTML = "Швидкість має бути більш ніж 0";
-    throw Error("Швидкість має бути більш ніж 0");
+  let cop = Number(document.getElementById("speed").value);
+  if (cop < 0) {
+    document.getElementById("res").innerHTML =
+      "копiйок не може бути меньш нiж нуль";
+    throw Error;
   }
-  if (time <= 0) {
-    document.getElementById("res").innerHTML = "час має бути більш ніж 0";
-    throw Error("час має бути більш ніж 0");
-  }
-
-  document.getElementById("res").innerHTML = h2res + res;
+  let grn = Math.trunc(cop / "100");
+  cop = cop % 100;
+  let res = grn + " гривень та " + cop + " копiйок";
+  document.getElementById("res").innerHTML = res;
 }
 button.addEventListener("click", v1);
